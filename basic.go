3ada5478498d99404674c8219a089ffcf3b2d3f1
fengchunsgit <1055688803@qq.com>
2019-06-25 21:26:52 +0800

    variable

--- basic.go
+++ basic.go
@@ -2,10 +2,18 @@ package main
 
 import "fmt"
 
+var (
+	aa=11
+	bb=333
+	cc="sss"
+)
 func main() {
 	fmt.Println("hello world")
 	variableZeroValue()
 	variableInitialValue()
+	variableTypeDeduction()
+	variableShorter()
+	fmt.Println(aa,bb,cc)
 }
 
 func variableZeroValue(){
@@ -18,4 +26,14 @@ func variableInitialValue(){
 	var  a,b int=3,4
 	var s string ="abc"
 	fmt.Println(a,b,s)
+}
+
+func variableTypeDeduction(){
+	var a,b,c,s=3,4,true,"def"
+	fmt.Println(a,b,c,s)
+}
+
+func variableShorter(){
+	a,b,c,s:=3,4,true,"def"
+	fmt.Println(a,b,c,s)
 }
